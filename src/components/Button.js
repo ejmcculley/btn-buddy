898ea2d2e287e0984a1309289c827ded585a9743
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-17 15:09:54 -0700

    Add proptypes to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Button = ({ color, text, onClick }) => {
   return (
     <>
@@ -14,6 +16,12 @@ const Button = ({ color, text, onClick }) => {
 Button.defaultProps = {
   color: '#101010',
   text: 'Select',
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button

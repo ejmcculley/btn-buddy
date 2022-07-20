1d0508073bc70029b808e5bb8566e7d8dcd777c3
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-20 14:34:13 -0700

    New logo added, media query troubleshooting

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ const Sidebar = () => {
     <>
       <div className="intro">
         <figure>
-          <img src='../../SVG/btnBuddyLogo3.svg' alt='btn Buddy Logo' />
+          <img src='../../SVG/btnBuddyLogo2.svg' alt='btn Buddy Logo' />
         </figure>
         <h1>Customization without Option Paralysis</h1>
         <p>Click through the button selector on your right. Copy the CSS at the end and add to your project!</p>

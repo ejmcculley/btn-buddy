d8e60d009b00c73dea80666aa1b6bc7dc12b4af7
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-20 18:52:18 -0700

    Create ProgressPanel component

---
+++ src/components/ProgressPanel.js
@@ -0,0 +1,17 @@
+const ProgressPanel = () => {
+  return (
+    <>
+      <div className="progressBar">
+        <h2>Shape</h2>
+        <h2>Color</h2>
+        <h2>Fill</h2>
+        <h2>Border</h2>
+        <h2>Shadow</h2>
+        <h2>Alignment</h2>
+        <h2>Hover</h2>
+      </div>
+    </>
+  )
+}
+
+export default ProgressPanel
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,11 @@
+import IntroContent from "./IntroContent"
 import Logo from "./Logo"
 
 const Sidebar = () => {
   return (
     <>
-      <div className="intro">
-        <Logo />
-        <h1>Customization without Option Paralysis</h1>
-        <p>Click through the button selector on your right.</p>
-        <p>Copy the CSS to your project!</p>
-      </div>
-      <div className="progress">
-
-      </div>
+      <Logo />
+      <IntroContent />
     </>
   )
 }

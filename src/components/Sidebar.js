3d3885900c0f6b1e3fb33833268be79736ad025e
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-20 18:54:20 -0700

    Add Logo, IntroContent, and ProgressPanel components to Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import IntroContent from "./IntroContent"
 import Logo from "./Logo"
+import ProgressPanel from "./ProgressPanel"
 
 const Sidebar = () => {
   return (
     <>
       <Logo />
       <IntroContent />
+      <ProgressPanel />
     </>
   )
 }

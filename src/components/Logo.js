ec6bb79eb47e1f586eb14b27b2590efc1268d90e
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-20 18:45:42 -0700

    Create Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <>
+      <figure>
+        <img src='../../SVG/btnBuddyLogo2.svg' alt='btn Buddy Logo' />
+      </figure>
+    </>
+  )
+}
+
+export default Logo

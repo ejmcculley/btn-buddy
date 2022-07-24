f9fbe531f965570faf15d4ad57d04f9de4b3ad3d
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-24 13:17:12 -0700

    Add text and onClick to Button component within Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,17 @@
 import Button from 'components/Button.js'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  let navigate = useNavigate();
   return (
     <>
-     <Button />
+    <h2>Let's Get Started!</h2>
+     <Button 
+      text={'Begin'}
+      onClick={() => {
+        navigate('/color');
+      }}
+     />
     </>
   )
 }

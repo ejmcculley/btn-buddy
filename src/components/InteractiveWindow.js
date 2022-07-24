cf68d041d97dcdf5e2e967e07574034310f2cc2f
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-24 12:09:20 -0700

    Remove btnBuild component and add functionality to interactiveWindow component

--- src/components/InteractiveWindow.js
+++ src/components/InteractiveWindow.js
@@ -1,11 +1,24 @@
-import BtnBuilder from "./BtnBuilder"
-import Results from "./Results"
+import BtnAlignment from "Pages/BtnAlignment"
+import BtnBorder from "Pages/BtnBorder"
+import BtnColor from "Pages/BtnColor"
+import BtnFill from "Pages/BtnFill"
+import BtnHover from "Pages/BtnHover"
+import BtnShadow from "Pages/BtnShadow"
+import BtnShape from "Pages/BtnShape"
+import Results from "Pages/Results"
+
 
 const InteractiveWindow = () => {
 
   return (
     <>
-      <BtnBuilder />
+      <BtnShape />
+      <BtnColor />
+      <BtnFill />
+      <BtnBorder />
+      <BtnShadow />
+      <BtnAlignment />
+      <BtnHover />
       <Results />
     </>
   )

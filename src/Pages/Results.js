f850ecc2b88885494c1c116786dbb974d879c13c
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-24 18:06:30 -0700

    Add Restart navigation to Results component

--- src/Pages/Results.js
+++ src/Pages/Results.js
@@ -1,6 +1,18 @@
+import Button from 'components/Button.js'
+import { useNavigate } from 'react-router-dom'
+
 const Results = () => {
+  let navigate = useNavigate();
   return (
-    <div>Results</div>
+    <>
+    <h2>Copy to Add to your Project!</h2>
+     <Button 
+      text={'Restart'}
+      onClick={() => {
+        navigate('/');
+      }}
+     />
+    </>
   )
 }
 

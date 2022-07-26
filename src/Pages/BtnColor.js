6f105a1a5e2555f8bc44c02efa27f76bc85f023b
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-25 18:33:07 -0700

    Install react-color, add ChromePicker component to BtnColor

--- src/Pages/BtnColor.js
+++ src/Pages/BtnColor.js
@@ -1,13 +1,20 @@
 import Button from "components/Button"
+import { useState } from "react";
+import { ChromePicker } from 'react-color';
 import { useNavigate } from 'react-router-dom'
 
 const BtnColor = () => {
+  const [color, setColor] = useState('#5750ff')
   let navigate = useNavigate();
   return (
     <>
     <h2>Choose your Color</h2>
+    <ChromePicker
+      color={color}
+      onChange={updatedColor => setColor(updatedColor.hex)}
+     />
      <Button 
-      text={'Nice Color!!!'}
+      color={color}
       onClick={() => {
         navigate('/shape');
       }}

b069f907cebe648ca71d5d9c0182bd9d3d4b7b46
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-30 19:11:36 -0700

    Add filled in and outline classes to BtnFill

--- src/Pages/BtnFill.js
+++ src/Pages/BtnFill.js
@@ -1,21 +1,21 @@
 import Button from "components/Button"
 import { useNavigate } from 'react-router-dom'
 
-const BtnFill = ({ color }) => {
+const BtnFill = () => {
   let navigate = useNavigate();
   return (
     <>
     <h2>Choose your Fill</h2>
      <Button 
       text={'Filled In'}
-      color={color}
+      className={'filled'}
       onClick={() => {
         navigate('/border');
       }}
      />
      <Button 
       text={'Outline'}
-      color={color}
+      className={'outline'}
       onClick={() => {
         navigate('/border');
       }}

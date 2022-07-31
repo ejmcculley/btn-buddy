f4904a83b63a773735e9cf03a16b9441dd9ff11a
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-07-30 18:25:44 -0700

    Add btnColor useState hook to update className to e.target.value of Button on click + navigate to next selector page

--- src/Pages/BtnColor.js
+++ src/Pages/BtnColor.js
@@ -1,31 +1,38 @@
 import Button from "components/Button"
 import { useNavigate } from 'react-router-dom'
-import BtnShape from "./BtnShape";
-import useAttribute from "useAttribute";
+import { useState } from "react";
 
-const BtnColor = ({ color, textColor }) => {
+
+const BtnColor = () => {
+  const [btnColor, setBtnColor] = useState('btn');
+  const handleAddColor = (e) => {
+    setBtnColor((btnColor) => e.target.value)
+  }
   let navigate = useNavigate();
+  
   return (
     <>
+    <style>
+      {
+        `
+        .red {background-color: red}
+        .green {background-color: green}
+        .blue {background-color: blue}
+        `
+      }
+    </style>
+
      <h2>Choose your Color</h2>
      
      <Button 
       text='Select'
-      color={color}
-      value={color}
-      textColor={textColor}
-      // set onClick to setBtnAttribute 
-      onClick={() => {
-      e => setBtnAttribute({ backgroundColor: e.target.value })
-      navigate('/shape');
-      }}
-     >
-      <BtnShape color={color} />
-     </Button>
-      
-   
+      className={`btn ${btnColor}`}
+      value={'blue'}
+      onClick={handleAddColor} {...navigate('/shape')}
+     />   
     </>
   )
 }
+
 
 export default BtnColor
